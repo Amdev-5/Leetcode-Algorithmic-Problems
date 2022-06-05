bc3f90a034083c8142c250f13fff2d81124b9a18
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-05 09:38:21 +0530

    Time: 212 ms (100.00%), Space: 82.9 MB (100.00%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
@@ -2,56 +2,11 @@ class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
       sort(nums.begin(),nums.end());
-      int idx = 0,count=1;
+      int curr_minIndex = 0,count=1;
       for(int i=1;i<nums.size();i++)
       {
-        if(nums[i]-nums[idx]>k)
-        {
-          count++;
-          idx=i;
-        }
+        if(nums[i]-nums[curr_minIndex]>k) count++,curr_minIndex = i;
       }
-      return count;
-      /*
-      vector<vector<int>> v;
-      vector<vector<int>> m;
-      v.push_back({nums[0]});
-      m.push_back({nums[0],nums[0]});
-      for(int i = 1;i<nums.size();i++)
-      {
-        bool flag = false;
-        for(int j = 0;j<v.size();j++)
-        {
-          if(nums[i]<m[j][1] && m[j][1]-nums[i]<=k)
-          {
-            m[j][0] = nums[i];
-            v[j].push_back(nums[i]);
-            flag = true;
-            break;
-          }
-          if(nums[i]>m[j][0] && nums[i]-m[j][0]<=k)
-          {
-            m[j][1] = nums[i];
-            v[j].push_back(nums[i]);
-            flag = true;
-            break;
-          }
-          if(nums[i]<=m[j][1] && nums[i]>=m[j][0])
-          {
-            v[j].push_back(nums[i]);
-            flag = true;
-            break;
-          }
-        }
-        if(flag==false)
-        {
-          v.push_back({nums[i]});
-          m.push_back({nums[i],nums[i]});
-          flag = true;
-        }
-      }
-      return v.size();
-      */
-        
+      return count; 
     }
 };
