7ecf1e75d30c9f93144d6df402adb3fb9d92caeb
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-08 18:18:29 +0530

    Time: 3 ms (65.44%), Space: 7 MB (75.44%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,6 +4,7 @@ public:
       if(nums.size()==1) return 1;
       int up =1;
       int down = 1;
+      // if current element is greater than last element than we need to update that up is down +1 viceversa
       for(int i=1;i<nums.size();i++)
       {
         if(nums[i]>nums[i-1]) up = down+1;
