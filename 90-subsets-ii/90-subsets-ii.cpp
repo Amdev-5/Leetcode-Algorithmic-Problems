f53ca89d1606d348223e521b756247ab669a64fb
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-15 18:56:01 +0530

    Time: 14 ms (17.95%), Space: 17.5 MB (10.08%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+  set<vector<int>> set;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+      rec(nums,{},nums.size());
+      vector<vector<int>> res;
+      for(auto t: set)
+        res.push_back(t);
+      //sort(res.begin(),res.end());
+      return res;
+      
+      
+    }
+  void rec(vector<int>& nums,vector<int> temp,int N)
+  {
+    if(N==0)
+    {
+      sort(temp.begin(),temp.end());
+      set.insert(temp);
+    }
+    else
+    {
+      rec(nums,temp,N-1);
+      temp.push_back(nums[N-1]);
+      rec(nums,temp,N-1);
+    }
+    
+  }
+};
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      /*
+        set<vector<int>> s;
+        unsigned int pow_set_size = pow(2, nums.size());
+        int counter, j;
+        for(counter = 0;counter<pow_set_size;counter++)
+        {
+            vector<int> temp;
+            for(j=0;j<nums.size();j++)
+            {
+                if(counter&(1<<j))
+                {
+                    temp.push_back(nums[j]);
+                    
+
+                }
+            }
+            s.insert(temp);
+        }
+        vector<vector<int>> res;
+        for (auto it = s.begin();
+         it != s.end();
+         it++) {
+  
+        res.push_back(*it);
+    }
+        return res;
+        */
+        
