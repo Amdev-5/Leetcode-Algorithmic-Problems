8a6d0205d5a85af3e4128687553b176c69430f71
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-16 12:46:32 +0530

    Time: 9 ms (25.34%), Space: 6 MB (80.38%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+      vector<int> temp;
+      int fact = 1;
+      for(int i = 1;i<n;i++)
+      {
+        fact = fact*i;
+        temp.push_back(i);
+      }
+      temp.push_back(n);
+      k = k-1;
+      string res = "";
+      while(true)
+      {
+        res = res+ to_string(temp[k/fact]);
+        temp.erase(temp.begin()+ k/fact);
+        if(temp.size()==0)
+        {
+          break;
+        }
+        k = k%fact;
+        fact = fact/temp.size();
+      }
+      return res;
+      
+    
+  }
+};
