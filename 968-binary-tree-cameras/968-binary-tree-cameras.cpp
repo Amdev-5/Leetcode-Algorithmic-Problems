797161eb8083c4ec38912ca77f4474e5ec1fe350
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-17 17:28:12 +0530

    Time: 14 ms (68.31%), Space: 21.1 MB (87.57%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -17,21 +17,25 @@ public:
   int camera = 0;
   int minCameraCover_(TreeNode* root)
   {
+    // child of leaves should say we are covered
     if(root==NULL) return 1;
     
+    // check left and right
     int lchild = minCameraCover_(root->left);
     int rchild = minCameraCover_(root->right);
-    
+    //if any of the child need camera then put camera and return 0 saying that camera has been implanted
     if(lchild==-1 || rchild==-1)
     {
       camera++;
       return 0;
     }
+    // if any of child has camera then return 1 saying i am covered
     if(lchild==0 || rchild==0) return 1;
-    
+    // return otherwise or for the root (critical case)
     return -1;
   }
     int minCameraCover(TreeNode* root) {
+      //this camera++ will happen for critical case with when root need to implant camera
       if(minCameraCover_(root)==-1) camera++;
       return camera;
     }
