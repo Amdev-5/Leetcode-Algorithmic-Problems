cbdd99c9eaf35ca4e5ca864fe62f7225bcb79306
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-25 00:41:36 +0530

    Time: 27 ms (28.49%), Space: 25.8 MB (48.06%) - LeetHub

---
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+      unordered_map<int,int> m;
+      for(int i = 0;i<postorder.size();i++)
+      {
+        m[postorder[i]] = i;
+      }
+      return build(preorder,0,preorder.size()-1,postorder,0,postorder.size()-1,m);
+        
+    }
+  TreeNode* build(vector<int>& pre,int preStart, int preEnd,vector<int>& post,int postStart, int postEnd, unordered_map<int,int>& m)
+  {
+    TreeNode* root = new TreeNode(pre[preStart]);
+    if(preStart==preEnd) return root;
+    int postRoot = m[pre[preStart+1]];
+    int len = postRoot-postStart+1;
+    root->left = build(pre,preStart+1,preStart+len,post,postStart,postStart+len-1,m);
+    if(postRoot+1==postEnd) return root;
+    root->right = build(pre,preStart+len+1,preEnd,post,postRoot+1,postEnd-1,m);
+    return root;
+  }
+};
