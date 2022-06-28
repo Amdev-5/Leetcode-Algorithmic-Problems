cd3ea2847ca5b9efa6c418abffe7a1f56a40a383
Amit Kumar <amitsingh16201814@gmail.com>
2022-06-28 21:49:08 +0530

    Time: 6 ms (10.05%), Space: 6.1 MB (43.88%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int percentageLetter(string s, char letter) {
-      long n = s.length();
-        long cnt = 0;
-        for(int i=0;i<n;i++){
-            if(s[i] == letter)
-                cnt++;
+      int n = s.size();
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            if(s[i] == letter) cnt++;
         }
-        
-        long x = cnt* 100 / n;
-        
-        return (int)x;
+        int ans = cnt * 100 / n;
+        return ans;
         
     }
 };
