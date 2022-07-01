7e972702ee9cd8969e203c7216a0259f052c2fdf
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-01 23:28:01 +0530

    Time: 187 ms (46.70%), Space: 39.9 MB (81.70%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+  int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+    vector<pair<int,int>> adj[n+1];
+    for(int i = 0;i<times.size();i++)
+    {
+      adj[times[i][0]].push_back({times[i][1],times[i][2]});
+    }
+    vector<int> dist(n+1,INT_MAX);
+    queue<int> q;
+    dist[k] = 0;
+    q.push(k);
+    while(!q.empty())
+    {
+      int z = q.front();
+      q.pop();
+      for(auto t: adj[z])
+      {
+        if(dist[z]+t.second<dist[t.first])
+        {
+          dist[t.first] = dist[z]+t.second;
+          q.push(t.first);
+        }
+      }
+    }
+    int res = 0;
+    for(int i = 1;i<=n;i++)
+    {
+      res = max(res,dist[i]);
+    }
+    if(res==INT_MAX) return -1;
+    return res;
+    
+  }
+};
+  /* Bellman Ford Algorithm
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> res(n+1,INT_MAX);
         res[k] = 0;
@@ -28,3 +62,4 @@ public:
         
     }
 };
+*/
