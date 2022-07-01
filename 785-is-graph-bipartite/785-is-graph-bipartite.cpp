43a10a65480a251d3ad3c1f6ec42445113796a21
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-01 11:45:22 +0530

    Time: 30 ms (76.45%), Space: 13.6 MB (60.05%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -3,6 +3,39 @@ public:
   bool isBipartite(vector<vector<int>>& graph) {
     int n =  graph.size();
     vector<int> color(n,-1);
+    queue<int> q;
+    for(int i=0;i<n;i++)
+    {
+      if(color[i]==-1)
+      {
+        q.push(i);
+        color[i] =1;
+        while(!q.empty())
+      {
+        int z = q.front();
+        q.pop();
+        if(color[z]==-1) color[z]=1;
+        for(auto t: graph[z])
+        {
+          if(color[t]==-1)
+          {
+            color[t]= 1-color[z];
+            q.push(t);
+          }
+          else if(color[t]!=-1 && color[t]==color[z]) return false;
+        }
+      }
+      }
+    }
+    return true;
+  }
+};
+/* DFS
+class Solution {
+public:
+  bool isBipartite(vector<vector<int>>& graph) {
+    int n =  graph.size();
+    vector<int> color(n,-1);
     for(int i = 0;i<n;i++)
     {
       if(color[i]==-1)
@@ -26,82 +59,5 @@ public:
     }
     return true;
   }
-};
-  /*
-    bool isBipartite(vector<vector<int>>& graph) {
-        vector<int> color (graph.size(),0);
-        bool visited[graph.size()];
-        for(int i = 0;i<graph.size();i++)
-        {
-            visited[i] = false;
-        }
-        queue<int> q;
-        for(int i = 0;i<graph.size();i++)
-        {
-            if(!visited[i])
-            {
-                q.push(i);
-        color[i] = 1;
-        visited[i] = true;
-        
-        while(!q.empty())
-        {
-            int z = q.front();
-            q.pop();
-            for(int i: graph[z])
-            {
-                if(!visited[i])
-                {
-                    q.push(i);
-                    visited[i] = true;
-                    if(color[z]==1)
-                        color[i] = -1;
-                    else
-                        color[i] = 1;
-                }
-                else
-                {
-                    if(color[i]==color[z])
-                    return false;
-                    
-                }
-            }
-        }
-                
-            }
-        }
-        //
-        q.push(i);
-        color[i] = 1;
-        visited[i] = true;
-        
-        while(!q.empty())
-        {
-            int z = q.front();
-            q.pop();
-            for(int i: graph[z])
-            {
-                if(!visited[i])
-                {
-                    q.push(i);
-                    visited[i] = true;
-                    if(color[z]==1)
-                        color[i] = -1;
-                    else
-                        color[i] = 1;
-                }
-                else
-                {
-                    if(color[i]==color[z])
-                    return false;
-                    
-                }
-            }
-        }
-        
-        
-        return true;
-        
-    }
 };
 */
