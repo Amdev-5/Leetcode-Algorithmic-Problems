7c844461f57c4ba35a8b066f2282741c032aac6d
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-05 03:16:38 +0530

    Time: 454 ms (38.73%), Space: 32.4 MB (48.55%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -2,6 +2,33 @@ class Solution {
 public:
   int findMaxForm(vector<string>& strs, int m, int n) {
     int len = strs.size();
+    int dp[len+1][m+1][n+1];
+    memset(dp,0,sizeof(dp));
+    //vector<vector<vector<int>>> dp(len+1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
+    for(int i = 1;i<=len;i++)
+    {
+      for(int j = 0;j<=m;j++)
+      {
+        for(int k = 0;k<=n;k++)
+        {
+          int count0= count(strs[i-1].begin(),strs[i-1].end(),'0');
+          int count1 = strs[i-1].size()-count0;
+          int res = dp[i-1][j][k];
+          if(j>=count0 && k>=count1)
+          {
+            res=max(1+dp[i-1][j-count0][k-count1],res);
+          }
+          dp[i][j][k] = res;
+        }
+      }
+    }
+    return dp[len][m][n];
+    
+  }
+};
+  /*
+  int findMaxForm(vector<string>& strs, int m, int n) {
+    int len = strs.size();
     vector<vector<vector<int>>> dp(len+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
     //bool dp[len+1][m+1][n+1];
     //memset(dp,0,sizeof(dp));
@@ -24,6 +51,7 @@ public:
   }
 
 };
+*/
   /*
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
