e829de510e75535f3561c37453b50a8fbba41839
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-05 00:51:40 +0530

    Time: 3 ms (97.68%), Space: 7.3 MB (39.59%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
   int BASE = 1000000;
-    int repeatedStringMatch(string A, string B) {
+  int repeatedStringMatch(string A, string B) {
+    /*
       if(A == B) return 1;
         int count = 1;
         string source = A;
@@ -13,34 +14,50 @@ public:
         if(Rabin_Karp(source,B) != -1) return count;
         if(Rabin_Karp(source+A,B) != -1) return count+1;
         return -1;
+        */
+    if(A==B) return 1;
+    int count = 1;
+    string source = A;
+    while(source.size()<B.size())
+    {
+      source+=A;
+      count++;
     }
-    int Rabin_Karp(string source, string target){
-        if(source == "" or target == "") return -1;
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
-        }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
-        }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
-            }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    return i-m+1;
-            }
-        }
-        return -1;
-      
-        
+    if(source==B) return count;
+    if(rab(source,B)!=-1) return count;
+    if(rab(source+A,B)!=-1) return count+1;
+    return -1;
+  }
+  int rab(string source, string target)
+  {
+    if(source=="" || target=="") return -1;
+    int m = target.size();
+    int power = 1;
+    for(int i = 0;i<m;i++)
+    {
+      power = (power*31)%BASE;
     }
+    int hashcode = 0;
+    int targetcode = 0;
+    for(int i =0;i<m;i++)
+    {
+      targetcode = (targetcode*31+target[i])%BASE;
+    }
+    for(int i = 0;i<source.size();i++)
+    {
+      hashcode = (hashcode*31+source[i])%BASE;
+      if(i<m-1) continue;
+      if(i>=m)
+      {
+        hashcode = (hashcode- source[i-m]*power)%BASE;
+      }
+      if(hashcode<0) hashcode+=BASE;
+      if(hashcode==targetcode)
+      {
+        if(source.substr(i-m+1,m)==target)
+          return i-m+1;
+      }
+    }
+    return -1;
+  }
 };
