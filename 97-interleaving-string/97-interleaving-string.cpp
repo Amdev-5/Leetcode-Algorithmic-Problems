b8516c030cc6ab103320e418a224497ce19f52f0
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-08 00:32:33 +0530

    Time: 11 ms (44.87%), Space: 6.7 MB (67.56%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
-  //bool res = false;
+  bool isInterleave(string s1, string s2, string s3) {
+    int m = s1.length();
+    int n = s2.length();
+    if(s3.length()!=m+n) return false;
+    vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+    dp[0][0] = true;
+    for(int i = 1;i<=m;i++)
+    {
+      if(s1[i-1]==s3[i-1]) dp[i][0] = dp[i-1][0];
+      else dp[i][0] = false;
+    }
+    for(int i = 1;i<=n;i++)
+    {
+      if(s2[i-1]==s3[i-1]) dp[0][i] = dp[0][i-1];
+      else dp[0][i] = false;
+    }
+    for(int i = 1;i<=m;i++)
+    {
+      for(int j=1;j<=n;j++)
+      {
+        dp[i][j] = false;
+        if(s1[i-1]==s3[i+j-1])
+        {
+          dp[i][j] = dp[i][j]|| dp[i-1][j];
+        }
+        if(s2[j-1]==s3[i+j-1])
+        {
+          dp[i][j] = dp[i][j] || dp[i][j-1];
+        }
+      }
+    }
+    return dp[m][n];
+    
+  }
+};
+  /*
   bool isInterleave(string s1, string s2, string s3) {
     int n1 = s1.length();
     int n2 = s2.length();
@@ -28,6 +63,7 @@ public:
     else return dp[i][j] = false;
   }
 };
+*/
   /*
   bool res = false;
   bool isInterleave(string s1, string s2, string s3) {
