51f84526ab778f304e5101f03cbd907cd9d58c5f
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-09 23:58:18 +0530

    Time: 66 ms (46.97%), Space: 38.9 MB (23.75%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -12,13 +12,20 @@
 class Solution {
 public:
   unordered_set<int> set;
+  bool findTarget(TreeNode* root, int k) {
+    if(root==NULL) return false;
+    if(set.count(k-root->val)!=0) return true;
+    set.insert(root->val);
+    return findTarget(root->left,k) || findTarget(root->right,k);
+  }
+};
+/*
+  unordered_set<int> set;
     bool findTarget(TreeNode* root, int k) {
       if(!root) return false;
       if(set.count(k-root->val)) return true;
       set.insert(root->val);
-      return findTarget(root->left,k) || findTarget(root->right,k);
-      
-      
-    
+      return findTarget(root->left,k) || findTarget(root->right,k); 
   }
 };
+*/
