62266f14db9fd60e17355e49134bc3199878147a
Amit Kumar <amitsingh16201814@gmail.com>
2022-07-09 19:03:53 +0530

    Time: 28 ms (51.37%), Space: 24.4 MB (15.97%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,6 +11,53 @@
  */
 class Solution {
 public:
+  int count = 0;
+  int kthSmallest(TreeNode* root, int k) {
+    stack<int> st;
+    pre(root,st);
+    while(count-k>0)
+    {
+      st.pop();
+      count--;
+    }
+    return st.top();
+    
+    
+    
+  }
+  void pre(TreeNode* root, stack<int>& st)
+  {
+    if(root==NULL) return;
+    pre(root->left,st);
+    st.push(root->val);
+    count++;
+    pre(root->right,st);
+  }
+};
+  /*
+  int kthSmallest(TreeNode* root, int k) {
+    stack<TreeNode*> st;
+    TreeNode* p = root;
+    while(p || !st.empty())
+    {
+      while(p)
+      {
+        st.push(p);
+        p = p->left;
+      }
+      p = st.top();
+      if(--k==0)
+        return p->val;
+      st.pop();
+      p = p->right;
+    }
+    return 0;
+  }
+};
+*/
+  
+  /*
+  
   vector<int> nums;
     int kthSmallest(TreeNode* root, int k) {
       dfs(root);
@@ -25,3 +72,4 @@ public:
     dfs(root->right);
   }
 };
+*/
